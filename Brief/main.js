a2b52e79c8adde91859682b312ab8229e55dd4ee
souhaillasfar <souhaildon007@gmail.com>
2024-11-01 16:05:34 +0100

    delete button

--- Brief/main.js
+++ Brief/main.js
@@ -12,15 +12,21 @@ button.addEventListener('click',function(){
 
 let btn = document.getElementById('btn');
 let text = document.getElementById('text');
-let description = document.getElementById('description')
+let description = document.getElementById('description');
 let date = document.getElementById('date');
 let priority = document.getElementById('priority');
 let todo = document.querySelectorAll('.box');
+let progres = document.querySelectorAll('.box1');
+let Done = document.querySelectorAll('.box2');
 let div = document.getElementById("div");
 
 btn.onclick = function(e){
-    e.preventDefault()
-    if(text.value != '' && description.value != '' && date.value != '' && priority.value != ''){
+    const status = document.getElementById('status');
+
+    e.preventDefault();
+
+     if (status.value == "to do" ) {
+        if(text.value != '' && description.value != '' && date.value != '' && priority.value != ''){
             todo[0].innerHTML += `
                 <div class="border-2 border-black p-3 mt-4" id="div">
        <h2>${text.value}</h2>
@@ -29,7 +35,7 @@ btn.onclick = function(e){
        <p>${date.value}</p>
       
        
-        <button class="bg-red-700 hover:bg-red-500 text-white pl-1 pr-1  rounded-lg" >Delete</button>
+        <button type="button"  onclick="this.parentElement.remove()"  class=" bg-red-700 hover:bg-red-500 text-white pl-1 pr-1  rounded-lg" >Delete</button>
         <button  class="bg-transparent hover:bg-blue-500 text-blue-700 font-mono hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded">Edit</button>
       </div>
              `
@@ -38,7 +44,53 @@ btn.onclick = function(e){
         date.value = ''
         priority.value = ''
         
-    }
+    };
 
+     }
+     else if (status.value == "in progress"){
+
+        if(text.value != '' && description.value != '' && date.value != '' && priority.value != ''){
+            progres[0].innerHTML += `
+                <div class="border-2 border-black p-3 mt-4" id="div">
+       <h2>${text.value}</h2>
+       <p>${description.value}</p>
+       <p>${priority.value}</p>
+       <p>${date.value}</p>
+      
+       
+        <button type="button"  onclick="this.parentElement.remove()"  class="bg-red-700 hover:bg-red-500 text-white pl-1 pr-1  rounded-lg" >Delete</button>
+        <button  class="bg-transparent hover:bg-blue-500 text-blue-700 font-mono hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded">Edit</button>
+      </div>
+             `
+        text.value = '' 
+        description.value = ''
+        date.value = ''
+        priority.value = ''
+        
+    };
+
+     } 
+     else if (status.value == "done"){
+     if(text.value != '' && description.value != '' && date.value != '' && priority.value != ''){
+            Done[0].innerHTML += `
+                <div class="border-2 border-black p-3 mt-4" id="div">
+       <h2>${text.value}</h2>
+       <p>${description.value}</p>
+       <p>${priority.value}</p>
+       <p>${date.value}</p>
+      
+       
+        <button type="button"  onclick="this.parentElement.remove()"  class="bg-red-700 hover:bg-red-500 text-white pl-1 pr-1  rounded-lg delete" >Delete</button>
+        <button  class="bg-transparent hover:bg-blue-500 text-blue-700 font-mono hover:text-white py-1 px-1 border border-blue-500 hover:border-transparent rounded">Edit</button>
+      </div>
+             `
+        text.value = '' 
+        description.value = ''
+        date.value = ''
+        priority.value = ''
+        
+    };
+     }
+     
 
 }
